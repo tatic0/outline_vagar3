6838d05cb3c30fc3afb5f76fbca651988c86b130
F Varas <fvaras@anevia.com>
2016-08-18 14:50:15 +0200

    conf function done, need to finish template

--- confwriter.py
+++ confwriter.py
@@ -1,6 +1,11 @@
-with open('FW_RTC_template.txt', 'r') as template:
-  a = template.read()
-conf = open('FW_RTC.txt', 'w')
-a = a.format("Y", "N", "", "", "", "", "", "" , "N", "", "", "", "", "", "" , "N", "", "", "", "", "", "" , "N", "", "", "" )
-conf.write(a)
-conf.close()
+
+def confmaker(model, options):
+  # model will be used to support the other Contour models
+  # as of now, only Roam3 is supported
+  with open('FW_RTC_template.txt', 'r') as template:
+    a = template.read()
+  conf = open('FW_RTC.txt', 'w')
+  #a = a.format("Y", "N", "", "", "", "", "", "" , "N", "", "", "", "", "", "" , "N", "", "", "", "", "", "" , "N", "", "", "" )
+  a = a.format(*options)
+  conf.write(a)
+  conf.close()
