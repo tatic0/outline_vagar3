cf60751a60792e610b1a3813152f5afbb2a36b1c
F Varas <fvaras@anevia.com>
2016-08-18 16:39:27 +0200

    added output path (unix only)

--- confwriter.py
+++ confwriter.py
@@ -1,10 +1,12 @@
 
-def confmaker(model, options):
+def confmaker(path, model, options):
   # model will be used to support the other Contour models
   # as of now, only Roam3 is supported
+  print("Camera Model: {}".format(model))
   with open('FW_RTC_template.txt', 'r') as template:
     a = template.read()
-  conf = open('FW_RTC.txt', 'w')
+  outputfile = path + "/" + "FW_RTC.txt"
+  conf = open(outputfile, 'w')
   # len(options) must be 28
   # date format: 2016/08/17 11:36:39
   a = a.format(*options)
